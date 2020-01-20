81ffac155330d33c808031d190a43db5ecffa7a4
leva84 <denfrut8@gmail.com>
2020-01-20 22:35:56 +0300

    Additional protection for user answer

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -34,7 +34,6 @@ class QuestionsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def load_question
       @question = Question.find(params[:id])
     end
@@ -43,8 +42,16 @@ class QuestionsController < ApplicationController
     reject_user unless @question.user == current_user
   end
 
-    # Only allow a trusted parameter "white list" through.
-    def question_params
+  # Мы говорим, что у хеша params должен быть ключ :question.
+  # Значением этого ключа может быть хеш с ключами: :user_id и :text.
+  # Другие ключи будут отброшены.
+  def question_params
+    # Защита от уязвимости: если текущий пользователь — адресат вопроса,
+    # он может менять ответы на вопрос, ему доступно и поле :answer.
+    if current_user.present? && params[:question][:user_id].to_i == current_user.id
       params.require(:question).permit(:user_id, :text, :answer)
+    else
+      params.require(:question).permit(:user_id, :text)
     end
+  end
 end
