2fbf8578f075343e621ca2bc1d81048052d03cbc
leva84 <denfrut8@gmail.com>
2020-01-28 22:08:02 +0300

    User statistics and fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,12 @@ class SessionsController < ApplicationController
   def create
     # используем метод authenticate, который мы уже писали
     # Передаём ему параметры email и password
-    @user = User.authenticate(params[:email], params[:password])
+    user = User.authenticate(params[:email], params[:password])
 
     # Если юзер есть, записываем id этого юзера в объект session
     # Перенаправляем на главную с сообщением
-    if @user.present?
-      session[:user_id] = @user.id
+    if user.present?
+      session[:user_id] = user.id
       redirect_to root_url, notice: 'вы успешно залогинились'
       # Если что-то не так – сообщаем и рендерим шаблон снова
     else
