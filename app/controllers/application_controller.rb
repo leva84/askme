40c672f9c74f8405564062421b7ebc2d50d43bff
leva84 <denfrut8@gmail.com>
2020-01-29 17:36:39 +0300

    Declension in users_helper and declension for nonresponse

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,8 @@
 class ApplicationController < ActionController::Base
 
-  helper_method :current_user, :reject_user, :declension
+  helper_method :current_user, :reject_user
 
   private
-
-  def declension(number, enot, enota, enotov)
-    indicator1 = number % 10
-    indicator2 = number % 100
-
-    if indicator2.between?(11, 14)
-      enotov
-    else
-      case indicator1
-      when 1
-        enot
-      when 2..4
-        enota
-      when 5..9, 0
-        enotov
-      end
-    end
-  end
-
   def reject_user
     redirect_to root_path, alert: 'Вам сюда низя!'
   end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,4 +8,22 @@ module UsersHelper
       asset_path 'avatar.jpg'
     end
   end
+
+  def declension(number, enot, enota, enotov)
+    indicator1 = number % 10
+    indicator2 = number % 100
+
+    if indicator2.between?(11, 14)
+      enotov
+    else
+      case indicator1
+      when 1
+        enot
+      when 2..4
+        enota
+      when 5..9, 0
+        enotov
+      end
+    end
+  end
 end
