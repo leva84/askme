983ebe59d4f1842ac2a108f12f4980c95d62f74f
leva84 <denfrut8@gmail.com>
2020-02-07 18:08:48 +0300

    Migrate 'add color to users'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_03_193457) do
+ActiveRecord::Schema.define(version: 2020_02_07_150327) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_02_03_193457) do
     t.string "password_hash"
     t.string "password_salt"
     t.string "avatar_url"
+    t.string "favorite_color"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
