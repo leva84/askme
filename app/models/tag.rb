02fa9826ceb79327bd66ab6f53611c34eb47af64
leva84 <denfrut8@gmail.com>
2020-03-05 13:29:30 +0300

    Add method 'normalaize_tag_name' in model Tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,12 @@
 class Tag < ApplicationRecord
   has_many :taggings, dependent: :destroy
   has_many :questions, through: :taggings, dependent: :destroy
+
+  before_save :normalaize_tag_name
+
+  private
+
+  def normalaize_tag_name
+    self.name.downcase!
+  end
 end
